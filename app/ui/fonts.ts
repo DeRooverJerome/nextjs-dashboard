793bc4c274d4a36080262bcb67635eaa280b4dc0
Jerome <derooverjdr@gmail.com>
2024-01-25 12:13:59 +0100

    initial

---
+++ app/ui/fonts.ts
@@ -0,0 +1,17 @@
+import { Lusitana } from 'next/font/google';
+export const lusitana = Lusitana({
+  weight: '400',
+  subsets: ['latin'],
+});
+
+import { Inter, Roboto_Mono } from 'next/font/google';
+
+export const inter = Inter({
+  subsets: ['latin'],
+  display: 'swap',
+});
+
+export const roboto_mono = Roboto_Mono({
+  subsets: ['latin'],
+  display: 'swap',
+});
